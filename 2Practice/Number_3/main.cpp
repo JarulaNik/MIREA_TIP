ea4238d04aaca988c16a91a651bbe310c03a2e80
Jaroslav Zhidkov <jaropolk1104@gmail.com>
2023-02-20 14:18:40 +0300

    Update main.cpp

--- 2Practice/Number_3/main.cpp
+++ 2Practice/Number_3/main.cpp
@@ -1,26 +1,44 @@
-#ifndef FIGURE_H
-#define FIGURE_H
+#include <iostream>
+#include "figure.h"
 
-class Figure {
-private:
-    float x1, x2, x3, x4;
-    float y1, y2, y3, y4;
-    float S, P;
+int main() {
 
-public:
-    Figure(float x1, float x2, float x3, float x4, float y1, float y2, float y3, float y4);
+    float x1, x2, x3, x4, y1, y2, y3, y4;
+    std::cout << "Enter the x and y coordinates of the four vertices of the first quadrilateral: ";
+    std::cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4;
+    Figure quadrilateral1(x1, x2, x3, x4, y1, y2, y3, y4);
 
-    void show();
+    std::cout << "Quadrilateral 1:\n";
+    quadrilateral1.show();
+    std::cout << "Is rectangle? " << std::boolalpha << quadrilateral1.is_Prug() << '\n';
+    std::cout << "Is square? " << std::boolalpha << quadrilateral1.is_Square() << '\n';
+    std::cout << "Is rhombus? " << std::boolalpha << quadrilateral1.is_Romb() << '\n';
+    std::cout << "Can be inscribed? " << std::boolalpha << quadrilateral1.is_In_Circle() << '\n';
+    std::cout << "Can inscribe? " << std::boolalpha << quadrilateral1.is_Out_Circle() << '\n';
 
-    bool is_Prug();
 
-    bool is_Square();
+    std::cout << "\nEnter the x and y coordinates of the four vertices of the second quadrilateral: ";
+    std::cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4;
+    Figure quadrilateral2(x1, x2, x3, x4, y1, y2, y3, y4);
 
-    bool is_Romb();
+    std::cout << "Quadrilateral 2:\n";
+    quadrilateral2.show();
+    std::cout << "Is rectangle? " << std::boolalpha << quadrilateral2.is_Prug() << '\n';
+    std::cout << "Is square? " << std::boolalpha << quadrilateral2.is_Square() << '\n';
+    std::cout << "Is rhombus? " << std::boolalpha << quadrilateral2.is_Romb() << '\n';
+    std::cout << "Can be inscribed? " << std::boolalpha << quadrilateral2.is_In_Circle() << '\n';
+    std::cout << "Can inscribe? " << std::boolalpha << quadrilateral2.is_Out_Circle() << '\n';
 
-    bool is_In_Circle();
 
-    bool is_Out_Circle();
-};
+    std::cout << "\nEnter the x and y coordinates of the four vertices of the third quadrilateral: ";
+    std::cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4;
+    Figure quadrilateral3(x1, x2, x3, x4, y1, y2, y3, y4);
 
-#endif
+    std::cout << "Quadrilateral 3:\n";
+    quadrilateral3.show();
+    std::cout << "Is rectangle? " << std::boolalpha << quadrilateral3.is_Prug() << '\n';
+    std::cout << "Is square? " << std::boolalpha << quadrilateral3.is_Square() << '\n';
+    std::cout << "Is rhombus? " << std::boolalpha << quadrilateral3.is_Romb() << '\n';
+    std::cout << "Can be inscribed? " << std::boolalpha << quadrilateral3.is_In_Circle() << '\n';
+    std::cout << "Can inscribe? " << std::boolalpha << quadrilateral3.is_Out_Circle() << '\n';
+}
